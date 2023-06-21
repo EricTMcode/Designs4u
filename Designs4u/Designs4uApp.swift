40cc173e762becb5431cb2ea11bad4b21c09cc35
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-21 14:47:10 +0200

    Initial Commit

---
+++ Designs4u/Designs4uApp.swift
@@ -0,0 +1,17 @@
+//
+//  Designs4uApp.swift
+//  Designs4u
+//
+//  Created by Eric on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Designs4uApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
